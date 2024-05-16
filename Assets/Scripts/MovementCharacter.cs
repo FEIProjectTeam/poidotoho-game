9dd3cb7970a1308a3826c90f48138221d4430e5b
Viktor Bojda <viktorbojda17@gmail.com>
2024-05-16 16:26:15 +0200

    fix: player kept moving after finding incident or time ended

--- Assets/Scripts/MovementCharacter.cs
+++ Assets/Scripts/MovementCharacter.cs
@@ -16,29 +16,39 @@ public class MovementCharacter : MonoBehaviour
 
     private void Update()
     {
-        // Detect left mouse button click
-        if (Input.GetMouseButtonDown(0) && GameManager.Instance.State == GameManager.GameState.RoamingMap)
+        if (GameManager.Instance.State != GameManager.GameState.RoamingMap)
         {
-            // Create a ray from the camera, passing through the mouse position
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            if (_agent.velocity != Vector3.zero)
+                _agent.ResetPath();
+            return;
+        }
 
-            // If the ray hits something on the Sidewalk layer
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, navmeshLayer))
-            {
-                // Set the agent's destination to the point where the ray hit
-                _agent.destination = hit.point;
+        if (!Input.GetMouseButtonDown(0))
+            return;
 
-                // Instantiate the ripple effect at the point where the ray hit the ground
-                ParticleSystem rippleEffectInstance = Instantiate(rippleEffectPrefab, hit.point, Quaternion.identity);
-                rippleEffectInstance.transform.forward = hit.normal; // Align with the surface normal
+        // Create a ray from the camera, passing through the mouse position
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-                // Play the particle system
-                rippleEffectInstance.Play();
+        // If the ray hits something with navmeshLayer
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, navmeshLayer))
+        {
+            // Set the agent's destination to the point where the ray hit
+            _agent.destination = hit.point;
 
-                // Optionally, destroy the particle system after it has finished
-                Destroy(rippleEffectInstance.gameObject, rippleEffectInstance.main.duration);
-            }
+            // Instantiate the ripple effect at the point where the ray hit the ground
+            ParticleSystem rippleEffectInstance = Instantiate(
+                rippleEffectPrefab,
+                hit.point,
+                Quaternion.identity
+            );
+            rippleEffectInstance.transform.forward = hit.normal; // Align with the surface normal
+
+            // Play the particle system
+            rippleEffectInstance.Play();
+
+            // Optionally, destroy the particle system after it has finished
+            Destroy(rippleEffectInstance.gameObject, rippleEffectInstance.main.duration);
         }
     }
 }
